ca9e5a0a913c7c5f1e60236262e45b6dd4ea7381
Agata <ag.kpp.37@gmail.com>
2018-03-06 17:41:29 +0100

    Update obliczenia.R

--- obliczenia.R
+++ obliczenia.R
@@ -1,5 +1,8 @@
-dane0 <- c(7,6,6,3,5,8,5,3,4,6,4...)
-dane1 <- c(8,7,6,7,5,5,6,5,5,3,3...)
+#dane0 - odpowiedzialna
+#dane1 - nieodpowiedzialna
+
+dane0 <- c(7,6,6,3,5,8,5,3,4,6,4,3,3,7,2,5,4,6,3)
+dane1 <- c(8,7,6,7,5,5,6,5,5,3,3,4,3,4,8)
 mean(dane0)
 mean(dane1)
 t.test (dane0, dane1)
