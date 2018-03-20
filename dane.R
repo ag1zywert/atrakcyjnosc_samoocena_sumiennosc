4056206b0b3a7acfe75a69dc601fd4e1356e00cd
Agata <ag.kpp.37@gmail.com>
2018-03-20 19:01:28 +0100

    Może tak może nie

--- dane.R
+++ dane.R
@@ -1,3 +1,13 @@
 library(tidyverse)
-data = dane_wlasciwe
+str(dane_wlasciwe)
+
+warunki <- group_by(dane_wlasciwe, Warunek)
+summarise (warunki, srednia = mean(Ocena), odchylenie = mad(Ocena))
+
+status_zwiazku <- group_by(dane_wlasciwe, status.zwiazku)
+summarise (status_zwiazku, srednia = mean(Ocena), odchylenie = mad(Ocena))
+
+ggplot(data = dane_wlasciwe) +
+  geom_point(mapping = aes(x = Wiek, y = Ocena, alpha = 3/10)) +
+  geom_smooth(mapping = aes(x = Wiek, y = Ocena))
 
